59c384612198fa1fd364d95c94ae1a9b301d1ae7
Vasile-Hij <vasile_hijs@yahoo.com>
2021-03-23 23:42:08 +0200

    fibonnaci_2_update

--- week4/curs8/3_fibonnaci/fibonnaci_2.py
+++ week4/curs8/3_fibonnaci/fibonnaci_2.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
 
-def main():
-    while True:
-        usr_input = input("Please insert a number: ")
-        if usr_input.isnumeric():
-            break
-        else:
-            print('The inserted %s is not a number!' % usr_input)
-            print('Try again with a number bigger or equal to 0: ')
-
-    n = int(usr_input)
-
+def fibonacci(n):
     previous_pos = 0
     current_num = 1
+
     if n < 0:
         print('Try again with a number bigger or equal to 0: ')
     elif n == 0:
@@ -20,12 +11,25 @@ def main():
     elif n == 1:
         return current_num
     else:
-        for _ in range(0, n):
+        for _ in range(0, n + 1):
             new_num = previous_pos + current_num
             previous_pos = current_num
             current_num = new_num
         return current_num
 
 
+def main():
+    while True:
+        usr_input = input("Please insert a number: ")
+        if usr_input.isnumeric():
+            break
+        else:
+            print('The inserted %s is range!' % usr_input)
+        continue
+
+    n = int(usr_input)
+    print(fibonacci(n))
+
+
 if __name__ == "__main__":
-    print(main())
+    main()
